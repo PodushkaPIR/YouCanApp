792cb37eb490302666ca2ab9f2dff161247dd21f
PodushkaPIR <seterovas@yandex.ru>
2023-01-24 22:42:36 +0700

    new splash screen implementation

--- app/src/main/java/com/example/youcan/MainActivity.kt
+++ app/src/main/java/com/example/youcan/MainActivity.kt
@@ -10,21 +10,38 @@ import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ExitToApp
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.rememberNavController
 import com.example.youcan.navigation.AppNavHost
 import com.example.youcan.navigation.NavRoute
 import com.example.youcan.ui.theme.YouCanTheme
 import com.example.youcan.utils.DB_TYPE
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        var isSplashScreen = mutableStateOf(true)
+
+        lifecycleScope.launch(Dispatchers.Default){
+            delay(500)
+            isSplashScreen.value = false
+        }
+        installSplashScreen().apply {
+            setKeepOnScreenCondition{
+                isSplashScreen.value
+            }
+        }
         setContent {
             YouCanTheme {
                 val context = LocalContext.current
--- app/src/main/java/com/example/youcan/navigation/AppNavHost.kt
+++ app/src/main/java/com/example/youcan/navigation/AppNavHost.kt
@@ -6,13 +6,11 @@ import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.youcan.MainViewModel
-import com.example.youcan.screens.AddScreen
-import com.example.youcan.screens.MainScreen
-import com.example.youcan.screens.NoteScreen
-import com.example.youcan.screens.StartScreen
+import com.example.youcan.screens.*
 import com.example.youcan.utils.Constants
 
 sealed class NavRoute(val route: String){
+//    object Splash: NavRoute(Constants.Screens.SPLASH_SCREEN)
     object Start: NavRoute(Constants.Screens.START_SCREEN)
     object Main: NavRoute(Constants.Screens.MAIN_SCREEN)
     object Add: NavRoute(Constants.Screens.ADD_SCREEN)
@@ -24,9 +22,18 @@ sealed class NavRoute(val route: String){
 fun AppNavHost(mViewModel: MainViewModel, navController: NavHostController){
 
     NavHost(navController = navController, startDestination = NavRoute.Start.route){
-        composable(NavRoute.Start.route){ StartScreen(navController = navController, viewModel = mViewModel) }
-        composable(NavRoute.Main.route){ MainScreen(navController = navController, viewModel = mViewModel) }
-        composable(NavRoute.Add.route){ AddScreen(navController = navController, viewModel = mViewModel) }
+//        composable(NavRoute.Splash.route){
+//            SplashScreen(navController = navController)
+//        }
+        composable(NavRoute.Start.route){
+            StartScreen(navController = navController, viewModel = mViewModel)
+        }
+        composable(NavRoute.Main.route){
+            MainScreen(navController = navController, viewModel = mViewModel)
+        }
+        composable(NavRoute.Add.route){
+            AddScreen(navController = navController, viewModel = mViewModel)
+        }
         composable(NavRoute.Note.route + "/{${Constants.Keys.ID}}"){ backStackEntry ->
             NoteScreen(navController = navController, viewModel = mViewModel, noteId = backStackEntry.arguments?.getString(Constants.Keys.ID))
         }
